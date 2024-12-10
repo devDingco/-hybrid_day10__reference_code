12ee735b0412bfddc568577fb04270f1cc24a059
devDingco <102280633+devDingco@users.noreply.github.com>
2024-12-10 14:26:00 +0900

    Add files via upload

---
+++ web/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import SignupButtonSection from "@/components/domains/login/\bsignup-section";
+import LoginSection from "@/components/domains/login/login-section";
+
+export default function LoginPage() {
+  return (
+    <main>
+      <LoginSection />
+      <SignupButtonSection />
+    </main>
+  );
+}
